6938ee4662218d84a43e15577e007dc59963db03
Simon Pinfold <mail@synap5e.info>
2014-09-13 15:30:23 +1200

    Added a script that can be used as a pre-commit hook to ensure code is not commited that does not compile

---
+++ check-compile.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+compiledir=`mktemp -d`
+cp src $compiledir/src -R
+cp lwjgl-2.9.1/jar $compiledir/lib -R
+cd $compiledir/src
+javac -cp "../lib/*" `find -name *.java`
+RESULT=$?
+#/usr/bin/rm $compiledir -R
+[ $RESULT -ne 0 ] && echo "Compile failed - disallowing commit" && exit 1
+echo "Compile succeeded - allowing commit" && exit 0
