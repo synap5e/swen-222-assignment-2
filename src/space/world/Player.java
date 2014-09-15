6a479f7072e69fe6f57c672652d6d252a1865818
libunamari <marialibunao@gmail.com>
2014-09-15 21:34:27 +1200

    Moved methods from mockplayer to player

--- src/space/world/Player.java
+++ src/space/world/Player.java
@@ -11,11 +11,60 @@ import space.world.items.Item;
 public class Player implements Entity,ViewablePlayer{
 	private Set<Item> inventory = new HashSet<Item>(); //items the player is carrying
 	private Vector2D position;
-	private Vector3D lookDir; 
-	private float eyeHeight;
 	private int id;
 	private int points;
-	
+	private static final float EYE_HEIGHT = 6;
+	private static final float JUMP_HEIGHT = 2;
+
+	private float jumpTime = 0;
+
+	private float xRotation = 90;
+	private float yRotation = 100;
+
+	public Vector2D getPosition() {
+		return position;
+	}
+
+	private static float DEGREES_TO_RADIANS(float degrees){
+		return (float) ((degrees) * (Math.PI / 180.0));
+	}
+
+	@Override
+	public Vector3D getLookDirection() {
+		float x_circ = (float) (Math.cos(DEGREES_TO_RADIANS(yRotation)) * Math.sin(DEGREES_TO_RADIANS(xRotation + 180)));
+		float y_circ = (float) (  										  Math.cos(DEGREES_TO_RADIANS(xRotation + 180)));
+		float z_circ = (float) (Math.sin(DEGREES_TO_RADIANS(yRotation)) * Math.sin(DEGREES_TO_RADIANS(xRotation + 180)));
+		return new Vector3D(x_circ, y_circ, z_circ);
+	}
+
+	@Override
+	public float getEyeHeight() {
+		return (float) ((EYE_HEIGHT+JUMP_HEIGHT) - JUMP_HEIGHT * Math.pow(jumpTime*2 - 1, 2));
+	}
+
+	private void moveLook(Vector2D mouseDelta) {
+		this.xRotation += mouseDelta.getY()/8f;
+		this.yRotation += mouseDelta.getX()/8f;
+
+		if (xRotation >= 360) xRotation = 360f;
+		if (xRotation <= 0) xRotation = 0f;
+	}
+
+	@Override
+	public float getAngle() {
+		return yRotation;
+	}
+
+	@Override
+	public float getElevation() {
+		return getEyeHeight()-EYE_HEIGHT;
+	}
+
+	@Override
+	public boolean canMove() {
+		return true;
+	}
+
 	public Player(Vector2D pos,int i){
 		position = pos;
 		id = i;
@@ -28,32 +77,11 @@ public class Player implements Entity,ViewablePlayer{
 	public void dropItem(Item i){
 		inventory.remove(i);
 	}
-	@Override
-	public Vector2D getPosition() {
-		return position;
-	}
 	
 	public void setPosition(Vector2D position) {
 		this.position = position;
 	}
-	
-	@Override
-	public Vector3D getLookDirection() {
-		return lookDir;
-	}
-	
-	public void setLookDirection(Vector3D lookDir) {
-		this.lookDir = lookDir;
-	}
-	
-	@Override
-	public float getEyeHeight() {
-		return eyeHeight;
-	}
-	
-	public void setEyeHeight(float eyeHeight) {
-		this.eyeHeight = eyeHeight;
-	}
+
 	public int getID() {
 		return id;
 	}
@@ -68,22 +96,5 @@ public class Player implements Entity,ViewablePlayer{
 	public Set<Item> getInventory(){
 		return inventory;
 	}
-	@Override
-	public float getAngle() {
-		// TODO do this properly
-		return 0;
-	}
-
-	@Override
-	public float getElevation() {
-		// TODO do this properly
-		return 0;
-	}
-
-	@Override
-	public boolean canMove() {
-		return true;
-	}
-
 	
 }
