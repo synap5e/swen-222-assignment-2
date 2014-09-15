a7e234052255b4f941a08020beb3faedb9a74d15
Jamigrith <james@greenwood.net.nz>
2014-09-16 00:18:22 +1200

    Made the client keep the connection to the server open, as well as provide a method to stop the client. Added additional javadoc.

--- src/space/network/Client.java
+++ src/space/network/Client.java
@@ -11,29 +11,44 @@ import space.world.Player;
 import space.world.World;
 
 /**
+ * The Client class manages the local version of the game state, 
+ *  applying changes from the server as well as from local input.
+ * Changes caused by local input are sent to the connected server.
+ * 
  * @author James Greenwood-Thessman (greenwjame1)
  */
 public class Client {
 
-	//TODO: Change to World
+	/**
+	 * The client's connection to the server.
+	 */
+	private Connection connection;
+	
+	//TODO: Change type to World
+	/**
+	 * The local version of the game world.
+	 */
 	private MockWorld world;
 	
+	/**
+	 * The player who is being played by the client.
+	 */
 	private Player localPlayer;
 	
 	/**
-	 * The last x coordinate for the mouse
+	 * The last x coordinate for the mouse.
 	 */
-	private int lastx = -1;
+	private int lastx;
 	
 	/**
-	 * The last y coordinate for the mouse
+	 * The last y coordinate for the mouse.
 	 */
-	private int lasty = -1;
+	private int lasty;
 	
 	/**
 	 * Creates a game client that connects to a server.
 	 * 
-	 * @param host the hostname of the server
+	 * @param host the host name of the server
 	 * @param port the port of the server
 	 * @param world the local instance of the game world
 	 * @param localPlayer the local player
@@ -42,17 +57,24 @@ public class Client {
 		this.world = world;
 		this.localPlayer = localPlayer;
 		
+		//Connect to the server
 		try {
-			Connection connection = new Connection(new Socket(host, port));
-			System.out.println(connection.readMessage());
-			connection.close();
+			connection = new Connection(new Socket(host, port));
 		} catch (IOException e) {
-			e.printStackTrace();
+			//Client failed to connect, critical failure
+			throw new RuntimeException(e);
 		}
 		
 		//Get the initial location of the mouse
 		lastx = Mouse.getX();
 		lasty = Mouse.getY();
+	}
+	
+	/**
+	 * Shuts down the client.
+	 */
+	public void shutdown(){
+		connection.close();
 	}
 	
 	/**
@@ -73,8 +95,17 @@ public class Client {
 		return null; //TODO Return the world object, once it is of the correct type.
 	}
 
+	/**
+	 * Updates the game world with changes from the server and local input.
+	 * 
+	 * @param delta the change in time since the last update
+	 */
 	public void update(int delta) {
-		//TODO: Apply updates from server
+		//Apply updates from server
+		while (connection.hasMessage()){
+			//TODO: Actually apply updates
+			System.out.println(connection.readMessage());
+		}
 		
 		world.update(delta);
 		updatePlayer(delta);
@@ -144,6 +175,7 @@ public class Client {
 	 * @param delta the change in time since the last update
 	 */
 	private void updateJump(int delta){
+		//TODO: implement jumping once methods to do so exist
 		/*float jumpTime = localPlayer.getJumpTime();
 		
 		if (Keyboard.isKeyDown(Keyboard.KEY_SPACE) && localPlayer.getJumpTime() == 0){
