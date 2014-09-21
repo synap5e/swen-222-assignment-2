3b308b35d3ab0a8092085244a59d19e7374e3a5e
Jamigrith <james@greenwood.net.nz>
2014-09-21 14:53:24 +1200

    Server now starts a game loop, which for now does not do anything

--- src/space/network/Server.java
+++ src/space/network/Server.java
@@ -5,18 +5,39 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.lwjgl.Sys;
+
+import space.gui.pipeline.mock.MockWorld;
+import space.gui.pipeline.viewable.ViewableRoom.LightMode;
+import space.math.Vector2D;
+import space.world.Player;
+import space.world.Room;
+import space.world.World;
+
+//TODO: Work out a way to let the server be shutdown nicely
 public class Server {
 	
 	private Thread connectionHandler;
+	private Thread gameLoop;
+	
 	private boolean stillAlive;
 	private ServerSocket socket;
-	private List<Connection> connections;
+	private Map<Integer, Connection> connections;
+	
+	//TODO change to world
+	private World world;
+	
+	//TODO: Work out better way of coming up with an ID
+	private int availableId = 9001;
 	
 	public Server(String host, int port){
 		//Create the list of client connections
-		connections = new ArrayList<Connection>();
+		connections = new HashMap<Integer, Connection>();
 		
 		//Create the socket for clients to connect to
 		try {
@@ -29,9 +50,19 @@ public class Server {
 		//Create Connection Handler
 		stillAlive = true;
 		connectionHandler = new Thread(new ConnectionHandler());
+		
+		//Load the World
+		//TODO: Load world from file
+		world = new World();
+		Room r = new Room(LightMode.DARK, 1, "temp", Arrays.asList(new Vector2D(-20, -20), new Vector2D(20, -20), new Vector2D(20, 20), new Vector2D(-20, 20)));
+		world.addRoom(r);
+		
+		//Start accepting connections
 		connectionHandler.start();
 		
-		//TODO: Make the server actually do stuff
+		//Start the game logic
+		gameLoop = new Thread(new ServerGameLoop());
+		gameLoop.start();
 	}
 	
 	public void shutdown(){
@@ -45,23 +76,67 @@ public class Server {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		for (Connection c : connections){
-			//TODO: use a better way to tell the client the server is shutting down
-			c.sendMessage("Server Shutting Down");
-			c.close();
+		synchronized (connections) {
+			for (Connection c : connections.values()){
+				//TODO: use a better way to tell the client the server is shutting down
+				c.sendMessage("Server Shutting Down");
+				c.close();
+			}
+		}
+	}
+	
+	private static long getTime() {
+		return (Sys.getTime() * 1000) / Sys.getTimerResolution();
+	}
+	
+	private class ServerGameLoop implements Runnable {
+		
+		@Override
+		public void run() {
+			long last = getTime();
+			while (stillAlive){
+				long now = getTime();
+				long delta = now - last;
+				
+				synchronized (connections) {
+					for (Map.Entry<Integer, Connection> cons : connections.entrySet()){
+						Connection con = cons.getValue();
+						int id = cons.getKey();
+						
+						while (con.hasMessage()){
+							//TODO: Actually apply updates
+							System.out.println(con.readMessage());
+							
+							//TODO: Send out updates to other clients
+						}
+					}
+				}
+				
+				//Update world TODO: World needs an update method that updates all updatable entities
+				//world.update(delta);
+				
+				last = now;
+			}
 		}
 	}
 	
 	private class ConnectionHandler implements Runnable {
-
+		
 		@Override
 		public void run() {
 			Socket socketConnection;
 			while(stillAlive){
 				try {
 					socketConnection = socket.accept();
+					int id = availableId++;
 					synchronized (connections) {
-						connections.add(new Connection(socketConnection));
+						connections.put(id, new Connection(socketConnection));
+					}
+					synchronized (world){
+						world.addEntity(new Player(new Vector2D(0, 0), id));
+						//TODO: Tell new client their ID.
+						
+						//TODO: Tell clients about new player
 					}
 				} catch (IOException e) {
 					System.err.println("Connection Attempt Failed");
@@ -69,5 +144,4 @@ public class Server {
 			}
 		}
 	}
-
 }
