dba6fc8137a72f258101939ef5188edace11f56e
Simon Pinfold <mail@synap5e.info>
2014-09-29 13:40:12 +1300

    Added test (test driven development!!!11ONE)

--- test/space/math/ConcaveHullTests.java
+++ test/space/math/ConcaveHullTests.java
@@ -1,6 +1,7 @@
 package space.math;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.junit.Test;
 
@@ -45,6 +46,23 @@ public class ConcaveHullTests {
 					-1,		-1
 				).getCentre(),
 				new Vector2D(0, 0));
+	}
+	
+	@Test
+	public void iteratorEqualsIteration(){
+		ConcaveHull h = createHull(
+				0,	1, 	
+				1,	1,
+				1,	0,
+				2,  0, 
+				0, 	2,
+				5,  1				
+			);
+		Iterator<Segment2D> it = h.iterator();
+		for (int i=0;i<h.size();i++){
+			assertEquals(it.next(), h.get(i));
+		}
+		assertFalse(it.hasNext());
 	}
 
 	private boolean hullContains(ConcaveHull hull, float x, float y) {
