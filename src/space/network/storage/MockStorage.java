0ffd653f0e92093bc10ba2d1a80051913e3e8b73
Jamigrith <james@greenwood.net.nz>
2014-10-01 16:01:09 +1300

    Added multiple rooms to mock world.

--- src/space/network/storage/MockStorage.java
+++ src/space/network/storage/MockStorage.java
@@ -26,6 +26,12 @@ public class MockStorage implements WorldLoader, WorldSaver {
 		world = new World();
 		Room r = new Room(LightMode.BASIC_LIGHT, 1, "temp", Arrays.asList(new Vector2D(-20, 20), new Vector2D(20, 20), new Vector2D(20, -20), new Vector2D(-20, -20)), new HashMap<Integer, List<Door>>());
 		world.addRoom(r);
+		Room r2 = new Room(LightMode.DARK, 2, "dark", Arrays.asList(new Vector2D(-20, -20), new Vector2D(20, -20), new Vector2D(20, -40), new Vector2D(-20, -40)), new HashMap<Integer, List<Door>>());
+		world.addRoom(r2);
+		Door d = new Door(new Vector2D(0, -20), 3, "door", r, r2, false, false);
+		r.addDoor(3, d);
+		r2.addDoor(1, d);
+		d.openDoor();
 	}
 
 	@Override
