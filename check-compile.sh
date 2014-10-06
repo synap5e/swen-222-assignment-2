3539bc6ffc1eae186b49c52111f9a60496e4563a
Simon Pinfold <mail@synap5e.info>
2014-10-06 21:45:04 +1300

    Allowed rendering of non

--- check-compile.sh
+++ check-compile.sh
@@ -4,7 +4,7 @@ compiledir=`mktemp -d`
 cp src $compiledir/src -R
 cp lib/lwjgl/jar $compiledir/lib -R
 cp lib/twl/TWL.jar $compiledir/lib/
-cd lib/json/json-simple-1.1.1.jar $compiledir/lib/
+cp lib/json/json-simple-1.1.1.jar $compiledir/lib/
 cd $compiledir/src
 javac -cp "../lib/*" `find -name *.java`
 RESULT=$?
