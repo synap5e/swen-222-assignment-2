5653982e0419f48b3b5dc0c8df7c46fc83b15522
libunamari <marialibunao@gmail.com>
2014-10-12 20:00:05 +1300

    Added Chair and Table

---
+++ src/space/world/Chair.java
@@ -0,0 +1,27 @@
+package space.world;
+
+import space.math.Vector2D;
+
+public class Chair extends Stationary {
+
+	public Chair(Vector2D position, int id, float elevation,
+			String description, String name) {
+		super(position, id, elevation, description, name);
+	}
+
+	@Override
+	public boolean canClip() {
+		return true;
+	}
+
+	@Override
+	public float getCollisionRadius() {
+		return 1;
+	}
+
+	@Override
+	public float getHeight() {
+		return 1;
+	}
+
+}
