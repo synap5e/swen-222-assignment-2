0881dec415c326de43eeebbefb2125aa86d4dd9b
Jamigrith <james@greenwood.net.nz>
2014-10-13 23:24:57 +1300

    Loaded teleporter and button models.

--- src/space/gui/pipeline/ModelFlyweight.java
+++ src/space/gui/pipeline/ModelFlyweight.java
@@ -50,11 +50,17 @@ public class ModelFlyweight {
 														new Vector3D(0.35f,0.35f,0.35f),
 														Material.bronze
 													));	
-			models.put("Teleporter", new WavefrontModel(	new File("./assets/models/teapot.obj"),
+			models.put("Teleporter", new WavefrontModel(	new File("./assets/models/teleporter.obj"),
 					new Vector3D(0, 0, 0),
 					new Vector3D(0, 90, 0), 0.5f,
 					new Vector3D(0.35f,0.35f,0.35f),
 					Material.bronze
+				));	
+			models.put("Button", new WavefrontModel(	new File("./assets/models/button.obj"),
+					new Vector3D(0, 0, 0),
+					new Vector3D(0, 90, 0), 1f,
+					new Vector3D(0.35f,0.35f,0.35f),
+					Material.simple
 				));	
 			
 			models.put("Bullet", new BulletModel());
