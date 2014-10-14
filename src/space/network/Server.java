438e490b0020858b0a4807e63ff99e0eaa2427a6
Jamigrith <james@greenwood.net.nz>
2014-10-14 17:41:57 +1300

    Documented the server.

--- src/space/network/Server.java
+++ src/space/network/Server.java
@@ -1,6 +1,5 @@
 package space.network;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
@@ -9,13 +8,10 @@ import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
-
 import org.lwjgl.Sys;
-
 import space.math.Vector2D;
 import space.network.message.DisconnectMessage;
 import space.network.message.DropPickupMessage;
@@ -31,7 +27,6 @@ import space.network.storage.WorldLoader;
 import space.network.storage.WorldSaver;
 import space.network.message.ShutdownMessage;
 import space.world.Container;
-import space.world.Door;
 import space.world.Entity;
 import space.world.Pickup;
 import space.world.Player;
@@ -39,6 +34,10 @@ import space.world.Room;
 import space.world.World;
 
 /**
+ * The Server class manages the shared version of the game state, 
+ *  applying updates to the world and applying any changes performed by a client.
+ *  Changes to the world are sent to all the clients (except to the client that caused the change).
+ *  While the server is running, clients are free to join and connect. 
  * 
  * @author James Greenwood-Thessman (300289004)
  */
@@ -49,25 +48,85 @@ public class Server {
 	 */
 	private static final int TIME_BETWEEN_SAVES = 300000;
 
+	/**
+	 * The default new save path.
+	 */
 	public static final String DEFAULT_SAVE = "save";
 	
+	/**
+	 * The path of default world definition. This is different from DEFAULT_SAVE as it cannot be written to.
+	 */
+	private static final String DEFAULT_WORLD = "default_world";
+	
+	/**
+	 * The thread that handles new connections.
+	 */
 	private Thread connectionHandler;
+	
+	/**
+	 * The thread that updates the game world, and applies messages sent from clients
+	 */
 	private Thread gameLoop;
+	
+	/**
+	 * The thread that saves the world to the specified save on a regular basis. The time between saves is specified by TIME_BETWEEN_SAVES.
+	 */
 	private Thread saveRoutine;
 	
+	/**
+	 * Whether the server is still running.
+	 */
 	private boolean stillAlive;
+	
+	/**
+	 * The socket that accepts new clients.
+	 */
 	private ServerSocket socket;
+	
+	/**
+	 * The map of connections. The map goes from the ID of the player to the connection to their client.
+	 */
 	private Map<Integer, Connection> connections;
 	
+	/**
+	 * The game world.
+	 */
 	private World world;
+	
+	/**
+	 * The map of players who no longer have connected clients. This is to allow players to rejoin.
+	 */
 	private Map<Integer, Player> inactivePlayers;
 	
+	/**
+	 * The set of used IDs. This is used for avoiding duplicate IDs.
+	 */
 	private Set<Integer> usedIds;
+	
+	/**
+	 * The generator of new IDs. IDs generated should be checked whether they are already in use.
+	 */
 	private Random idGenerator;
 	
+	/**
+	 * The saver of worlds.
+	 */
 	private WorldSaver saver;
+	
+	/**
+	 * The path of the save file.
+	 */
 	private String savePath;
 	
+	/**
+	 * Create and start a new server.
+	 * 
+	 * @param host the host to accept clients on
+	 * @param port the port to accept clients on
+	 * @param loader the loader that will load save file
+	 * @param saver the saver that will save the world
+	 * @param savePath the path of the save file. If the default world location it will be changes to a different path.
+	 */
 	public Server(String host, int port, WorldLoader loader, WorldSaver saver, String savePath){
 		//Create the list of client connections
 		connections = new HashMap<Integer, Connection>();
@@ -90,7 +149,7 @@ public class Server {
 		connectionHandler = new Thread(new ConnectionHandler());
 		
 		//Make sure not to have the default world overwritten
-		if (savePath.equals("default_world")){
+		if (savePath.equals(DEFAULT_WORLD)){
 			savePath += "2";
 		}
 		
@@ -103,7 +162,7 @@ public class Server {
 			loader.loadWorld(savePath);
 		} catch (Exception e){
 			//If something went wrong assume the file didn't exist
-			loader.loadWorld("default_world");
+			loader.loadWorld(DEFAULT_WORLD);
 		}
 		world = loader.getWorld();
 		//Mark the IDs of all the entities in the world as used
@@ -134,6 +193,9 @@ public class Server {
 		saveRoutine.start();
 	}
 	
+	/**
+	 * Stops the server. The server will send out a shutdown message to all connected clients before closing all connections and the socket.
+	 */
 	public void shutdown(){
 		//Stop the connection handler
 		stillAlive = false;
@@ -146,9 +208,11 @@ public class Server {
 		try {
 			socket.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			//No need to do anything as the server is no longer going to be used
 			e.printStackTrace();
 		}
+		
+		//Disconnect the clients
 		synchronized (connections) {
 			for (Connection c : connections.values()){
 				try {
@@ -160,12 +224,16 @@ public class Server {
 		}
 	}
 	
+	/**
+	 * Handles a client disconnecting. The disconnect does not need to be expected.
+	 * 
+	 * @param disconnectedID the ID of the client that has disconnected
+	 */
 	private void handleDisconnect(int disconnectedID){
 		Player p = (Player) world.getEntity(disconnectedID);
 		
 		//Remove player from world
 		world.getRoomAt(p.getPosition()).removeFromRoom(p);
-		//world.removeEntity(p); TODO add world.removeEntity(Entity e)
 		
 		//Keep track of the player allowing for reconnects
 		inactivePlayers.put(disconnectedID, p);
@@ -192,10 +260,12 @@ public class Server {
 		}
 	}
 	
-	private static long getTime() {
-		return (Sys.getTime() * 1000) / Sys.getTimerResolution();
-	}
-	
+	/**
+	 * Sends a message to all connections except the ID specified.
+	 * 
+	 * @param connectionID the ID of the connection not to send the message to.
+	 * @param message the message to send
+	 */
 	private void sendMessageToAllExcept(int connectionID, Message message){
 		for (Map.Entry<Integer, Connection> cons : connections.entrySet()){
 			if (cons.getKey() != connectionID){
@@ -209,6 +279,16 @@ public class Server {
 	}
 	
 	/**
+	 * Gets the current time.
+	 * 
+	 * @return The current time.
+	 */
+	private static long getTime() {
+		return (Sys.getTime() * 1000) / Sys.getTimerResolution();
+	}
+	
+	/**
+	 * ServerGameLoop when run, updates the world and applies any actions that clients have done.
 	 * 
 	 * @author James Greenwood-Thessman (300289004)
 	 */
@@ -218,17 +298,22 @@ public class Server {
 		public void run() {
 			long last = getTime();
 			while (stillAlive){
+				//Get the change in time
 				long now = getTime();
 				int delta = (int) (now - last);
 				
+				//Create a map of connections that will not be updated
 				Map<Integer,Connection> connections = new HashMap<Integer, Connection>();
 				synchronized (connections) {
 					connections.putAll(Server.this.connections);
 				}
+				
+				//For each connection
 				for (Map.Entry<Integer, Connection> cons : connections.entrySet()){
 					Connection con = cons.getValue();
 					int id = cons.getKey();
 					try {
+						//Try to read any available message
 						while (!con.isClosed() && con.hasMessage()){
 							Message message = con.readMessage();
 
@@ -340,6 +425,7 @@ public class Server {
 	}
 	
 	/**
+	 * ConnectionHandler, when run, accepts new clients and syncs the world with them.
 	 * 
 	 * @author James Greenwood-Thessman (300289004)
 	 */
@@ -347,14 +433,13 @@ public class Server {
 		
 		@Override
 		public void run() {
-			Socket socketConnection;
-			Connection newClient;
+			//While the server is still running
 			while(stillAlive){
 				try {
-					socketConnection = socket.accept();
+					Socket socketConnection = socket.accept();
 					
 					//Create the connection for the new client
-					newClient = new Connection(socketConnection);
+					Connection newClient = new Connection(socketConnection);
 					//Get the previous ID of the client
 					int id = ((PlayerJoiningMessage) newClient.readMessage()).getPlayerID();
 					
@@ -376,7 +461,7 @@ public class Server {
 						//Assign a new ID
 						while (usedIds.contains(id = idGenerator.nextInt(1000)));
 						usedIds.add(id);
-						p = new Player(new Vector2D(0, 0), id, "Player"); //TODO use name
+						p = new Player(new Vector2D(0, 0), id, "Player " + id);
 						p.setTorch(false);
 					}
 					
@@ -412,10 +497,12 @@ public class Server {
 						
 					}
 				} catch (SocketException se){
+					//An error with a single client connecting does not affect the rest of the server so no need to take action
 					if (!se.getMessage().equals("socket closed")){
 						System.err.println("Socket Failure");
 					}
 				} catch (IOException e) {
+					//An error with a single client connecting does not affect the rest of the server so no need to take action
 					System.err.println("Connection Attempt Failed");
 				}
 			}
@@ -423,6 +510,7 @@ public class Server {
 	}
 	
 	/**
+	 * WorldSaveRoutine, when run, saves the world periodically.
 	 * 
 	 * @author James Greenwood-Thessman (300289004)
 	 */
