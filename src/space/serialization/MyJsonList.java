e4a6663ff1642893a2c75855b2f3ed61f4e87f87
whitesweta <whitesweta@yahoo.co.uk>
2014-10-15 02:13:47 +1300

    added javadoc

--- src/space/serialization/MyJsonList.java
+++ src/space/serialization/MyJsonList.java
@@ -6,51 +6,101 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 /**
- * Represents a MyJsonList for classes and entities in the game
- * this is a wrapper class for the raw JSONArray from the library
+ * Represents a MyJsonList for classes and entities in the game this is a
+ * wrapper class for the raw JSONArray from the library
  *
- * It enables type safety rather than being generic - to avoid accidently put the wrong data type
+ * It enables type safety rather than being generic - to avoid accidently put
+ * the wrong data type
  *
  * @author Shweta Barapatre
  *
  */
 
-public class MyJsonList implements Iterable<JSONObject>{
+public class MyJsonList implements Iterable<JSONObject> {
 	JSONArray list;
 
-	public MyJsonList(){
+	/**
+	 * constructor
+	 */
+	public MyJsonList() {
 		list = new JSONArray();
 	}
 
+	/**
+	 * constructor that takes an already loaded list
+	 * 
+	 * @param loadedList
+	 *            JSONArray
+	 */
 	public MyJsonList(JSONArray loadedList) {
 		list = loadedList;
 	}
 
-	public void add(String toAdd){
+	/**
+	 * add a string to myjsonlist
+	 * 
+	 * @param toAdd
+	 *            string to add
+	 */
+	public void add(String toAdd) {
 		list.add(toAdd);
 	}
 
-	public void add(double toAdd){
+	/**
+	 * add a number to the myjsonlist
+	 * 
+	 * @param toAdd
+	 *            value to add
+	 */
+	public void add(double toAdd) {
 		list.add(toAdd);
 	}
 
-	public void add(boolean toAdd){
+	/**
+	 * add a boolean to myjsonlist
+	 * 
+	 * @param toAdd
+	 *            boolean to add
+	 */
+	public void add(boolean toAdd) {
 		list.add(toAdd);
 	}
 
-	public void add(MyJsonObject toAdd){
-		 list.add(toAdd.getRawObject());
+	/**
+	 * add a myjsonobject to myjsonlist
+	 * 
+	 * @param toAdd
+	 *            myjsonobject to add
+	 */
+	public void add(MyJsonObject toAdd) {
+		list.add(toAdd.getRawObject());
 	}
 
-	public void  add(MyJsonList toAdd){
+	/**
+	 * add a myjsonlist to myjsonlist
+	 * 
+	 * @param toAdd
+	 *            myjsonlist to add
+	 */
+	public void add(MyJsonList toAdd) {
 		list.add(toAdd.getRawList());
 	}
 
+	/**
+	 * returns original raw JSONArray
+	 * 
+	 * @return JSONArray
+	 */
 	public Object getRawList() {
 		return list;
 	}
 
-	public int getSize(){
+	/**
+	 * returns size of the list
+	 * 
+	 * @return int size
+	 */
+	public int getSize() {
 		return list.size();
 	}
 
@@ -59,13 +109,35 @@ public class MyJsonList implements Iterable<JSONObject>{
 		return list.iterator();
 	}
 
+	/**
+	 * gets a myjsonlist from the specified index
+	 * 
+	 * @param i
+	 *            index
+	 * @return myjsonlist at index
+	 */
 	public MyJsonList getMyJsonList(int i) {
 		return new MyJsonList((JSONArray) list.get(i));
 	}
+
+	/**
+	 * returns a number from myjsonlist at that index
+	 * 
+	 * @param i
+	 *            index
+	 * @return value
+	 */
 	public double getNumber(int i) {
 		return (double) list.get(i);
 	}
 
+	/**
+	 * returns myjsonobject from specified index
+	 * 
+	 * @param i
+	 *            index
+	 * @return myjsonobject
+	 */
 	public MyJsonObject getMyJsonObject(int i) {
 		return new MyJsonObject((JSONObject) list.get(i));
 	}
--- src/space/serialization/SaveFileNotValidException.java
+++ src/space/serialization/SaveFileNotValidException.java
@@ -1,5 +1,12 @@
 package space.serialization;
 
+/**
+ * A exception class that is thrown when the save file is not valid or formatted
+ * correctly
+ * 
+ * @author Shweta Barapatre (300287438)
+ *
+ */
 public class SaveFileNotValidException extends Exception {
 	public SaveFileNotValidException() {
 		super();
